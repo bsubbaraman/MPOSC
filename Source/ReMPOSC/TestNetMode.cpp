276c10b2c9e2dffbd6e901d46ed67652abf3547d
bsubbaraman <remap@remap-blair.local>
2020-02-23 12:51:12 -0800

    playback control progress

---
+++ Source/ReMPOSC/TestNetMode.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestNetMode.h"
+
+// Sets default values
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FTestDelegate);
+
+ATestNetMode::ATestNetMode()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestNetMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestNetMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    if (GetNetMode()== NM_Standalone)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "standalone");
+    }
+    else if (GetNetMode()== NM_DedicatedServer)
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "dedicated");
+    }
+    else
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "not stand");
+
+    }
+   
+
+}
+
+
+
